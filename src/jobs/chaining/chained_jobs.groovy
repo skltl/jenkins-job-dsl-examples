4358fb85350705ec2dafd66a46001189264ebf52
skltl <as118410@gmail.com>
2019-06-29 20:26:49 +0200

    tried creating chained jobs

---
+++ src/jobs/chaining/chained_jobs.groovy
@@ -0,0 +1,31 @@
+package jobs.chaining
+
+
+
+1..4.each {it ->
+    job("example3-chaining/job$it") {// here we use interpolation
+        description("job$it")
+        steps {
+            shell("echo \"job $it\"")
+        }
+        publishers {
+            downstream('job' + getIncremented(it), 'SUCCESS')// either SUCCESS, UNSTABLE OR FAILURE
+        }
+
+    }
+}
+
+job("example3-chaining/job5") {
+    description("job5")
+    steps {
+        shell('echo "job 5 - no more downstream!"')
+    }
+
+}
+
+queue('example3-chaining/job1')// start building first job!
+
+
+def getIncremented(i){
+    return i + 1
+}
