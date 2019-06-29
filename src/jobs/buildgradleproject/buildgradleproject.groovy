9e4e44763600f41cef87f4e6a1679461eb6112b4
skltl <as118410@gmail.com>
2019-06-29 17:08:36 +0200

    overwriting tag creation on pull (why would anyone want that anyway?!)

--- src/jobs/buildgradleproject/buildgradleproject.groovy
+++ src/jobs/buildgradleproject/buildgradleproject.groovy
@@ -5,7 +5,7 @@ job("example2_building_gradle_project/download-and-build-gradle-project"){
 
     description("download and build gradle projject")
     scm {
-        git('https://github.com/skltl/helloWorld.git')
+        git('https://github.com/skltl/helloWorld.git', {node -> node / 'extensions' << '' })
     }
     steps {
         gradle('clean build')
