3286d482e3c014666a2c59577c32a3950997b541
skltl <as118410@gmail.com>
2019-06-29 14:29:28 +0200

    added script in package to see what happens

---
+++ src/main/groovy/emptypackagedjob/emptyPackagedJob.groovy
@@ -0,0 +1,5 @@
+package emptypackagedjob
+
+job('example1_empty_packaged_job/new-empty-packaged-job') {// 'example1_empty_job' is a folder name, 'new-empty-job' is a job name; we didn't want to create a new job in the root path
+    description('description')
+}
