ee87e9ce44e33ee0b31771bdf399b95fe52e06c2
skltl <as118410@gmail.com>
2019-06-30 16:01:42 +0200

    tried to make parametrized build

---
+++ src/jobs/passingparams/passing_params_to_jobs.groovy
@@ -0,0 +1,58 @@
+
+
+def mockBranchNames = ['feature/example-1', 'feature/example-2', 'feature/example-3']
+
+mockBranchNames.each {branch ->
+    createBuildJob(this, branch)
+    createAcceptanceTestJob(this, branch)
+}
+
+
+def createBuildJob(dslFactory, branchName){
+
+    dslFactory.job("example5-passing_params/$branchName-build") {// here we use interpolation
+        description("$branchName")
+        triggers {
+            cron('*/5 * * * *')
+        }
+        steps {
+            shell("echo \"mocking build $branchName\"")
+
+            downstreamParameterized {
+
+                trigger('example5-passing_params/$branchName-test') {
+                    parameters {
+                        booleanParam('RUN_TESTS', true)
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
+
+
+def createAcceptanceTestJob(dslFactory, branchName){
+
+    dslFactory.job("example5-passing_params/$branchName-test") {// here we use interpolation
+        description("$branchName")
+        parameters {
+            booleanParam('RUN_TESTS', true, 'uncheck to disable tests')
+        }
+        steps {
+            shell("echo \"should I run mock acceptance test for $branchName\"? ${getRunTestsValue()}")
+        }
+
+    }
+}
+
+
+def getRunTestsValue(){
+    binding.variables.each {
+        if(it.key.equals("RUN_TESTS")){
+            return Boolean.valueOf(it.value)
+        }
+    }
+}
