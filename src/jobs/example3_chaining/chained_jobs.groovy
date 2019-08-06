ef6c215ce515f9059e5d06a97189327aca1178d1
skltl <as118410@gmail.com>
2019-08-06 19:13:24 +0200

    renamed third example

--- src/jobs/example3_chaining/chained_jobs.groovy
+++ src/jobs/example3_chaining/chained_jobs.groovy
@@ -1,25 +1,21 @@
-package jobs.chaining
-
-
-
 (1..4).each{jobNumber ->
 
 
     println "building job $jobNumber..."
-    job("example3-chaining/job$jobNumber") {// here we use interpolation
+    job("example3_chaining/job$jobNumber") {// here we use interpolation
         description("job$jobNumber")
         steps {
             shell("echo \"job $jobNumber\"")
         }
         publishers {
-            downstream("example3-chaining/job${jobNumber + 1}", 'SUCCESS')// either SUCCESS, UNSTABLE OR FAILURE
+            downstream("example3_chaining/job${jobNumber + 1}", 'SUCCESS')// either SUCCESS, UNSTABLE OR FAILURE
         }
 
     }
 }
 
 // the last job won't downstream anything
-job("example3-chaining/job5") {
+job("example3_chaining/job5") {
     description("job5")
     steps {
         shell('echo "job 5 - no more downstream!"')
