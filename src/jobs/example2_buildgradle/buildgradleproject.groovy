64f946f54d6a39467bac0838baf2974e8589d49f
skltl <as118410@gmail.com>
2019-08-06 18:58:57 +0200

    renamed example 2

---
+++ src/jobs/example2_buildgradle/buildgradleproject.groovy
@@ -0,0 +1,12 @@
+
+
+job("example2_buildgradle/download-and-build-gradle-project"){
+
+    description("download and build gradle project")
+    scm {
+        git('https://github.com/skltl/helloWorld.git', {node -> node / 'extensions' << '' })// helloWorld.git is another project, from different repo altogether
+    }
+    steps {
+        gradle('clean build', '', true){node -> node/ 'makeExecutable' << 'true'}
+    }
+}
