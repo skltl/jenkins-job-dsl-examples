9391081c447df94a073ff893fdadbd363c06674d
skltl <as118410@gmail.com>
2019-08-06 18:38:16 +0200

    renamed first example job, deleted packaged job

--- src/main/groovy/emptyJob.groovy
+++
@@ -1,3 +0,0 @@
-job('example1_empty_job/new-empty-job') {// 'example1_empty_job' is a folder name, 'new-empty-job' is a job name; we didn't want to create a new job in the root path
-    description('description')
-}
---
+++ src/main/groovy/example1_emptyJob.groovy
@@ -0,0 +1,5 @@
+// copy and paste it into Jenkins
+
+job('example1_empty_job/example1_emptyJob.groovy') {// 'example1_empty_job' is a folder name, 'example1_emptyJob' is a job name; we didn't want to create a new job in the root path
+    description('description')
+}
