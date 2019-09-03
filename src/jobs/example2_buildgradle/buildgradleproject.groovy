03007afa97d8b3ddca2b01f4516f290edc659ce9
Trzasalska <julita.trzasalska@capgemini.com>
2019-09-03 19:06:48 +0200

    simplified example 2 a bit

--- src/jobs/example2_buildgradle/buildgradleproject.groovy
+++ src/jobs/example2_buildgradle/buildgradleproject.groovy
@@ -4,9 +4,9 @@ job("example2_buildgradle/download-and-build-gradle-project"){
 
     description("download and build gradle project")
     scm {
-        git('https://github.com/skltl/helloWorld.git', {node -> node / 'extensions' << '' })// helloWorld.git is another project, from different repo altogether
+        git('https://github.com/skltl/helloWorld.git', {xmlNode -> xmlNode / 'extensions' << '' })// helloWorld.git is another project, from different repo altogether
     }
     steps {
-        gradle('clean build', '', true){node -> node/ 'makeExecutable' << 'true'}
+        gradle('clean build', '', true)
     }
 }
